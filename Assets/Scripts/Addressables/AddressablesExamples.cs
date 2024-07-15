a3a69142e902cc5deb25debdc926d346d4af445f
Paul Miell <101790408+PaxRon777@users.noreply.github.com>
2024-07-15 16:19:27 +0200

    Update AddressablesExamples.cs

--- Assets/Scripts/Addressables/AddressablesExamples.cs
+++ Assets/Scripts/Addressables/AddressablesExamples.cs
@@ -6,14 +6,26 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AddressablesExamples : MonoBehaviour
 {
-    [SerializeField] private AssetReference _assetReferenceCube;    
+    [SerializeField] private AssetReference _assetReferenceCube;
     [SerializeField] private AssetLabelReference _labelReference;
-   
+    [SerializeField] private AssetReferenceGameObject _gameObject;
+
+    private GameObject _gameObjectCube;
+
     void Start()
     {
         _assetReferenceCube.LoadAssetAsync<GameObject>().Completed += LoadAsset; //Load an asset of type GameObject
         Addressables.LoadAssetAsync<GameObject>(_labelReference).Completed += LoadLabelReference; //Load asset by its label
-    }    
+        _gameObject.InstantiateAsync().Completed += LoadCube;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _gameObject.ReleaseInstance(_gameObjectCube); //Removes the AssetReferenceGameObject cube
+        }
+    }
 
     private void LoadAsset(AsyncOperationHandle<GameObject> gameObject)
     {
@@ -26,7 +38,7 @@ public class AddressablesExamples : MonoBehaviour
             Debug.Log("Asset load error!");
         }
     }
-    
+
     private void LoadLabelReference(AsyncOperationHandle<GameObject> gameObject)
     {
         if (gameObject.Result != null)
@@ -38,5 +50,10 @@ public class AddressablesExamples : MonoBehaviour
         {
             Debug.Log("Asset Reference load error!");
         }
+    }
+
+    private void LoadCube(AsyncOperationHandle<GameObject> gameObject)
+    {
+        _gameObjectCube = gameObject.Result;
     }
 }
