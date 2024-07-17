69e7a2b126afa34c0eb22bd59ead64ec6b85850e
Paul Miell <101790408+PaxRon777@users.noreply.github.com>
2024-07-17 10:23:42 +0200

    Update PlayerController.cs

--- Assets/Scripts/Input System/PlayerController.cs
+++ Assets/Scripts/Input System/PlayerController.cs
@@ -1,36 +1,54 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+//Input System and Input Actions example
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 5;
     [SerializeField] private float _jumpStrength = 15;
 
+    //Assign Input Action variables
     private InputAction _moveAction;
     private InputAction _jumpAction;
+
     private Vector2 _move;
     private Rigidbody _rb;
+    private bool _isJumping;
 
     void Start()
     {
-        _moveAction = InputSystem.actions.FindAction("move");
-        _jumpAction = InputSystem.actions.FindAction("jump");
+        //Assign your Input Action references
+        _moveAction = InputSystem.actions.FindAction("Move");
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+
         _rb = GetComponent<Rigidbody>();
     }
 
-
     void Update()
     {
+        //Read the player move input
         _move = _moveAction.ReadValue<Vector2>();
+
+        //Move the player left or right on the X axis
         transform.position = new Vector3(transform.position.x + (_move.x * Time.deltaTime * _speed), transform.position.y, transform.position.z);
 
-    }
-
-    private void FixedUpdate()
-    {
         if (_jumpAction.IsPressed())
         {
+            _isJumping = true;
+        }
+        else
+        {
+            _isJumping = false;
+        }
+    }
+
+    //Jump by adding up force to the RigidBody
+    private void FixedUpdate()
+    {
+        if (_isJumping)
+        {
             _rb.AddForce(Vector3.up * _jumpStrength);
-        }       
+        }
     }
 }
